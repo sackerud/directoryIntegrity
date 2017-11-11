fdec63d30c1b350a42d46c03f9d6bbd178d7abc2
josa <jonas.sackerudfahlen@acino.se>
2017-11-12 00:42:38 +0100

    Added new extension method and enum for comparing filesystem structures

---
+++ src/directoryIntegrity.Core/CompareExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace directoryIntegrity.Core
+{
+    public static class CompareExtension
+    {
+        public static IEnumerable<FileSystemEntryComparison> CompareTo(this IEnumerable<FileSystemEntry> referenceEntries,
+                                                                            IEnumerable<FileSystemEntry> currentEntries)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/directoryIntegrity.Core/Enums.cs
@@ -0,0 +1,10 @@
+﻿namespace directoryIntegrity.Core
+{
+    public enum FileSystemEntryComparisonResult
+    {
+        NotInitialized = 0,
+        Intact = 1,
+        Moved = 2,
+        Removed = 3
+    }
+}
