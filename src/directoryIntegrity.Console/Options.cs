65df42d60770917558db8f6c76b9d706d0d82ecf
josa <jonas.sackerudfahlen@acino.se>
2018-05-07 22:44:41 +0200

    New console app project

---
+++ src/directoryIntegrity.Console/Options.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+
+namespace directoryIntegrity.ConsoleApp
+{
+    [Verb("createref", HelpText = "Create a reference file which represents a snapshot of a directory")]
+    internal class CreateReferenceFileOptions
+    {
+        [Option('d', "dirtoscan", HelpText = "The directory to create a shapshot of")]
+        public string DirectoryToScan { get; set; }
+
+        [Option('o', "outputpath", HelpText = "Location to where the reference file will be stored")]
+        public string ReferenceFilepath { get; set; }
+    }
+
+    [Verb("scan", HelpText = "Scan a directory for changes compared to a reference file")]
+    internal class ScanOptions
+    {
+        [Option('r',
+                "referencefile",
+                Required = false, 
+                HelpText = "The reference file used for comparison with the scan of a directory"]
+        public string ReferenceFile { get; set; }
+
+        [Option('d', "dirtoscan", HelpText = "The directory to create a shapshot of")]
+        public string DirectoryToScan { get; set; }
+    }
+}
