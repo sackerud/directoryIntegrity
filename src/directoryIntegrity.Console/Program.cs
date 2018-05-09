9d1bb0b46f421abc66c6f26b5a3b793ef225163e
xxsackej <ext.jonas.sackerudfahlen@tieto.com>
2018-05-09 16:44:24 +0200

    * New class that reads a reference file and spits out IEnumerable<IFileSystemEntry>  * Ignore case when comparing file paths

--- src/directoryIntegrity.Console/Program.cs
+++ src/directoryIntegrity.Console/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using CommandLine;
+using directoryIntegrity.Core;
+using directoryIntegrity.Core.FileSystem;
 using directoryIntegrity.Core.ReferenceFile;
 using directoryIntegrity.Core.Scan;
 using Newtonsoft.Json;
+using Directory = System.IO.Directory;
+using File = System.IO.File;
 
 namespace directoryIntegrity.ConsoleApp
 {
@@ -30,7 +33,6 @@ namespace directoryIntegrity.ConsoleApp
 
         private static int HandleParseError(IEnumerable<Error> errs)
         {
-            Console.WriteLine(string.Join(Environment.NewLine, errs.Select(e => e.Tag)));
             return -1;
         }
 
@@ -60,8 +62,48 @@ namespace directoryIntegrity.ConsoleApp
                 return -1;
             }
 
+            if (!File.Exists(opts.ReferenceFile))
+            {
+                Console.WriteLine($"{opts.ReferenceFile} does not exist");
+            }
+
             _scanOptions = opts;
+
+            var scanner = new DirectoryScanner(_scanOptions.DirectoryToScan);
+            var scanResult = scanner.Scan();
+
+            var referenceFileContents = new JsonReferenceFileReader().Read(_scanOptions.ReferenceFile);
+
+            var comparison = scanResult.FirstOrDefault().CompareTo(referenceFileContents.FirstOrDefault());
+
+            PrintComparison(comparison);
+
             return 0;
+        }
+
+        private static void PrintComparison(IEnumerable<FileSystemEntryComparison> comparison)
+        {
+            Console.WriteLine($@"Intact folders: {comparison.Count(c => c.Result == FileSystemEntryComparisonResult.Intact
+                                                                       && c.ReferenceFileSystemEntry.IsDirectory)}");
+            Console.WriteLine($@"Intact files: {comparison.Count(c => c.Result == FileSystemEntryComparisonResult.Intact
+                                                                        && !c.ReferenceFileSystemEntry.IsDirectory)}");
+
+            Console.WriteLine("The following files and directories has been removed:");
+
+            foreach (var fseComparison in comparison.Where(c => c.Result == FileSystemEntryComparisonResult.Removed))
+            {
+                Console.WriteLine($"{fseComparison.ReferenceFileSystemEntry.Path}");
+            }
+
+            Console.WriteLine("The following files and directories may have been moved:");
+            foreach (var fseComparison in comparison.Where(c => c.Result == FileSystemEntryComparisonResult.Moved))
+            {
+                Console.WriteLine($"{fseComparison.ReferenceFileSystemEntry.Path}");
+                foreach (var movedFse in fseComparison.CurrentFileSystemEntries)
+                {
+                    Console.WriteLine($"\t=>{movedFse.Path}");
+                }
+            }
         }
     }
 }
