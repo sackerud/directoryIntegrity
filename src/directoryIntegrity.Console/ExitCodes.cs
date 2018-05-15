f1da10aca8b9f6e8b3a8c955af27d5bffa3a323d
xxsackej <ext.jonas.sackerudfahlen@tieto.com>
2018-05-15 10:31:38 +0200

    Moved ExitCode från Program.cs to a separate file

---
+++ src/directoryIntegrity.Console/ExitCodes.cs
@@ -0,0 +1,11 @@
+﻿namespace directoryIntegrity.ConsoleApp
+{
+    public class ExitCodes
+    {
+        public const int RefFileDoesNotExist = 400;
+        public const int ScanDirDoesNotExist = 404;
+        public const int InvalidArgs = 501;
+
+        public const int Success = 0;
+    }
+}
--- src/directoryIntegrity.Console/Program.cs
+++ src/directoryIntegrity.Console/Program.cs
@@ -154,13 +154,4 @@ namespace directoryIntegrity.ConsoleApp
             }
         }
     }
-
-    public class ExitCodes
-    {
-        public const int RefFileDoesNotExist = 400;
-        public const int ScanDirDoesNotExist = 404;
-        public const int InvalidArgs = 501;
-
-        public const int Success = 0;
-    }
 }
